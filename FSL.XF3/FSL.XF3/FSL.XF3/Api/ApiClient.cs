a137f7e190b48823fd370b9ab4d0c364010b3ca7
Fabio Silva Lima <fabiosilvalima@terra.com.br>
2017-05-08 07:47:17 -0300

    POST methods

--- FSL.XF3/FSL.XF3/FSL.XF3/Api/ApiClient.cs
+++ FSL.XF3/FSL.XF3/FSL.XF3/Api/ApiClient.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using Newtonsoft.Json;
 using FSL.XF3.Api.Models;
 using Newtonsoft.Json.Converters;
+using System.Text;
 
 namespace FSL.XF3.Api
 {
@@ -51,6 +52,61 @@ namespace FSL.XF3.Api
             AddReponseHeaders();
 
             var response = await _restClient.GetAsync(_restClient.BaseAddress);
+            response.EnsureSuccessStatusCode();
+            var data = response.Content.ReadAsStringAsync().Result;
+
+            return data;
+        }
+
+        public async Task<BaseApiResult<TModel>> PostAsync<TModel>(string apiRoute, object body = null, Action<BaseApiResult<TModel>> callback = null)
+        {
+            try
+            {
+                var json = await PostAsync(apiRoute, body);
+                var data = JsonConvert.DeserializeObject<TModel>(json, GetConverter());
+                var result = new OkApiResult<TModel>(data);
+
+                callback?.Invoke(result);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return new InvalidApiResult<TModel>(ex);
+            }
+        }
+
+        public async Task<BaseApiResult<TModel>> PostResultAsync<TModel>(string apiRoute, object body = null, Action<BaseApiResult<TModel>> callback = null)
+        {
+            try
+            {
+                var data = await PostAsync(apiRoute, body);
+                var result = JsonConvert.DeserializeObject<OkApiResult<TModel>>(data, GetConverter());
+
+                callback?.Invoke(result);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return new InvalidApiResult<TModel>(ex);
+            }
+        }
+
+        private async Task<string> PostAsync(string apiRoute, object body)
+        {
+            var url = _apiUrlBase + "/" + apiRoute;
+
+            _restClient = _restClient ?? new HttpClient();
+            _restClient.BaseAddress = new Uri(url);
+
+            ClearReponseHeaders();
+            AddReponseHeaders();
+
+            var bodySerialize = JsonConvert.SerializeObject(body);
+            StringContent content = new StringContent(bodySerialize, Encoding.UTF8, "application/json");
+
+            var response = await _restClient.PostAsync(_restClient.BaseAddress, content);
             response.EnsureSuccessStatusCode();
             var data = response.Content.ReadAsStringAsync().Result;
 
--- FSL.XF3/FSL.XF3/FSL.XF3/Api/IApiClient.cs
+++ FSL.XF3/FSL.XF3/FSL.XF3/Api/IApiClient.cs
@@ -8,5 +8,7 @@ namespace FSL.XF3.Api
     {
         Task<BaseApiResult<T>> GetAsync<T>(string apiRoute, Action<BaseApiResult<T>> callback = null);
         IApiClient UseSufix(string urlSufix);
+        Task<BaseApiResult<TModel>> PostResultAsync<TModel>(string apiRoute, object body = null, Action<BaseApiResult<TModel>> callback = null);
+        Task<BaseApiResult<TModel>> PostAsync<TModel>(string apiRoute, object body = null, Action<BaseApiResult<TModel>> callback = null);
     }
 }
